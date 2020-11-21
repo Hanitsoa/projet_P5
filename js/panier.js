c849e10851d8abbe40ece4363588423da88962ac
Hanitsoa <61844504+Hanitsoa@users.noreply.github.com>
2020-11-21 11:08:59 +0100

    ajout designationProduit panier

--- js/panier.js
+++ js/panier.js
@@ -8,7 +8,22 @@ let produitElt = sessionStorage.getItem("cart");
 console.log(produitElt);
 produitElt = JSON.parse(produitElt);
         console.log(produitElt);
+var div = document.createElement("div");
+div.className = "designation";
+item.appendChild(div);
 
+var images = document.createElement("p");
+images.textContent = "images";
+div.appendChild(images);
+var noms = document.createElement("p");
+noms.textContent = "noms";
+div.appendChild(noms);
+var lenses = document.createElement("p");
+lenses.textContent = "lenses";
+div.appendChild(lenses);
+var prix = document.createElement("p");
+prix.textContent = "prix";
+div.appendChild(prix);
 
 function afficher(produit){
 	
@@ -25,6 +40,10 @@ function afficher(produit){
     var nameElt = document.createElement("p");
     nameElt.textContent = produit.name;
     div.appendChild(nameElt);
+
+    var lensesElt = document.createElement("p");
+    lensesElt.textContent = produit.lenses;
+    div.appendChild(lensesElt);
 
     var priceElt = document.createElement("p");
     priceElt.textContent = produit.price +" "+"€";
