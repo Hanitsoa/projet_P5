f5782826536b31de1c9830ab2e94785d29eb0911
Hanitsoa <61844504+Hanitsoa@users.noreply.github.com>
2021-01-13 20:00:35 +0100

    Organisation code panier

--- js/panier.js
+++ js/panier.js
@@ -81,21 +81,20 @@ displayTotalPrice()
 
 var subtotal = document.querySelector(".subtotal");
 
-
-    var formValid = document.getElementById("bouton_envoi");
-        var mail = document.getElementById("courriel");
-        var missMail = document.getElementById("missMail");
-        var mailValid = /^[a-zA-Z0-9._-]+@[a-zA-Z-]+\.[a-zA-Z]{2,3}$/
-        var missPrenom = document.getElementById("missPrenom");
-        var prenomValid = /^[A-Z][a-z]+([-A-Z][a-z]*)*$/
-        var missNom = document.getElementById("missNom");
-        var nomValid = /^[A-Z][a-z]+$/
-        var missAdresse = document.getElementById("missAdresse");
-        var adresseValid = /^[0-9]{1,4}[\s][a-zA-Z][a-z]+([\s][A-Za-z]*)*$/
-        var missVille = document.getElementById("missVille");
-        var villeValid = /^[A-Z][a-z]+$/
-        var missCodePostale = document.getElementById("missCodePostale");
-        var codePostaleValid = /^[0-9]{5}$/
+var formValid = document.getElementById("bouton_envoi");
+var mail = document.getElementById("courriel");
+var missMail = document.getElementById("missMail");
+var mailValid = /^[a-zA-Z0-9._-]+@[a-zA-Z-]+\.[a-zA-Z]{2,3}$/
+var missPrenom = document.getElementById("missPrenom");
+var prenomValid = /^[A-Z][a-z]+([-A-Z][a-z]*)*$/
+var missNom = document.getElementById("missNom");
+var nomValid = /^[A-Z][a-z]+$/
+var missAdresse = document.getElementById("missAdresse");
+var adresseValid = /^[0-9]{1,4}[\s][a-zA-Z][a-z]+([\s][A-Za-z]*)*$/
+var missVille = document.getElementById("missVille");
+var villeValid = /^[A-Z][a-z]+$/
+var missCodePostale = document.getElementById("missCodePostale");
+var codePostaleValid = /^[0-9]{5}$/
 
             // [miniscule Majuscule 0à9 ._-]
             // + veut dire peuvent repeter autant de fois
@@ -103,115 +102,20 @@ var subtotal = document.querySelector(".subtotal");
             // /^ debut d'un regex,3}
             // $/ la fin d'un regex
 
-        formValid.addEventListener("click", function(event){
-                validationMail(event);
-                validationPrenom(event);
-                validationNom(event);
-                validationAdresse(event);
-                validationVille(event);
-                validationCodePostale(event);
-
-        });
-
-        function validationMail(event){
-            console.log(courriel.value);//valeur input
-            console.log(mailValid.test(courriel.value));// type boolean, renvoie true ou false//
-            console.log(courriel.validity.valueMissing);
-            if (courriel.validity.valueMissing){
-                event.preventDefault();
-                missMail.textContent = "Mail manquant";
-            }else if (mailValid.test(courriel.value) == false){
-                event.preventDefault();
-                missMail.textContent = "Format incorrect";
-                }else{
-                    missMail.textContent = "";
-                }
-        }
-
-        function validationPrenom(event){
-            event.preventDefault();
-            if (prenom.validity.valueMissing){
-                event.preventDefault();
-                missPrenom.textContent = "Prénom manquant";
-            console.log(missPrenom.textContent);
-            }else if (prenomValid.test(prenom.value) == false){
-                event.preventDefault();
-                missPrenom.textContent = "Format incorrect";
-            }else{
-                    missPrenom.textContent = "";
-                }
-        }
-    
-        function validationNom(event){
-            event.preventDefault();
-            if (nom.validity.valueMissing){
-                event.preventDefault();
-                missNom.textContent = "Nom manquant";
-            console.log(missNom.textContent);
-            }else if (nomValid.test(nom.value) == false){
-                event.preventDefault();
-                missNom.textContent = "Format incorrect";
-            }else{
-                    missNom.textContent = "";
-                }
-        }
-
-        function validationAdresse(event){
-            event.preventDefault();
-            if (adresse.validity.valueMissing){
-                event.preventDefault();
-                missAdresse.textContent = "adresse manquant";
-            console.log(missAdresse.textContent);
-            }else if (adresseValid.test(adresse.value) == false){
-                event.preventDefault();
-                missAdresse.textContent = "Format incorrect";
-            }else{
-                    missAdresse.textContent = "";
-                }
-        }
-
-        function validationVille(event){
-            event.preventDefault();
-            if (ville.validity.valueMissing){
-                event.preventDefault();
-                missVille.textContent = "Ville manquant";
-            console.log(missVille.textContent);
-            }else if (villeValid.test(ville.value) == false){
-                event.preventDefault();
-                missVille.textContent = "Format incorrect";
-            }else{
-                    missVille.textContent = "";
-                }
-        }
-
-        function validationCodePostale(event){
-            event.preventDefault();
-            if (codePostale.validity.valueMissing){
-                event.preventDefault();
-                missCodePostale.textContent = "Code postale manquant";
-            console.log(missCodePostale.textContent);
-            }else if (codePostaleValid.test(codePostale.value) == false){
-                event.preventDefault();
-                missCodePostale.textContent = "Format incorrect";
-            }else{
-                    let cart = sessionStorage.getItem("cart")
-                    cart = JSON.parse(cart);
-                    missCodePostale.textContent = "";
-                    alert("Votre commande a bien prise en compte !");
-                    sessionStorage.clear();
-                    nom.value="";
-                    prenom.value="";
-                    courriel.value="";
-                    adresse.value="";
-                    ville.value="";
-                    codePostale.value="";
-                    var acheter = document.querySelector(".container");
-                    acheter.textContent="";
-                    
-                    var subtotal = document.querySelector(".subtotal");
-                    subtotal.textContent ="";
-                }
-        }
+formValid.addEventListener("click", function(event){
+    let validators = [] 
+    validators.push(validationMail(event));
+    validators.push(validationPrenom(event));
+    validators.push(validationNom(event));
+    validators.push(validationAdresse(event));
+    validators.push( validationVille(event));
+    validators.push(validationCodePostale(event));
+    console.log(validators);
+                       
+    if (validators.every((value) => value === true )) {
+        passerCommande();
+    }        
+});
 
 function supprimer(event){
     console.log(event.target.dataset.index);
@@ -243,3 +147,124 @@ function displayTotalPrice(){
     subtotal.textContent = "Total :"+" "+ prix +" "+"€";
 }
 
+function validationMail(event){
+    event.preventDefault();
+    if (courriel.validity.valueMissing){
+        missMail.textContent = "Mail manquant";
+        return false;
+    } else if (mailValid.test(courriel.value) == false){
+        missMail.textContent = "Format incorrect";
+        return false;
+    } else {
+        missMail.textContent = "";
+    }
+    return true;
+}
+function validationNom(event){
+    event.preventDefault();
+    if (nom.validity.valueMissing){
+        event.preventDefault();
+        missNom.textContent = "Nom manquant";
+        return false;
+    } else if (nomValid.test(nom.value) == false){
+        event.preventDefault();
+        missNom.textContent = "Format incorrect";
+        return false;
+    } else {
+            missNom.textContent = "";
+        }
+    return true;
+}
+function validationPrenom(event){
+    event.preventDefault();
+    if (prenom.validity.valueMissing){
+        event.preventDefault();
+        missPrenom.textContent = "Prénom manquant";
+        return false;
+    } else if (prenomValid.test(prenom.value) == false){
+        event.preventDefault();
+        missPrenom.textContent = "Format incorrect";
+        return false;
+    } else {
+            missPrenom.textContent = "";
+        }
+    return true;
+}
+function validationAdresse(event){
+    event.preventDefault();
+    if (adresse.validity.valueMissing){
+        event.preventDefault();
+        missAdresse.textContent = "adresse manquant";
+        return false;
+    console.log(missAdresse.textContent);
+    } else if (adresseValid.test(adresse.value) == false){
+        event.preventDefault();
+        missAdresse.textContent = "Format incorrect";
+        return false;
+    } else {
+        missAdresse.textContent = "";
+        }
+    return true;
+}
+function validationVille(event){
+    event.preventDefault();
+    if (ville.validity.valueMissing){
+        event.preventDefault();
+        missVille.textContent = "Ville manquant";
+        return false;
+    console.log(missVille.textContent);
+    } else if (villeValid.test(ville.value) == false){
+        event.preventDefault();
+        missVille.textContent = "Format incorrect";
+        return false;
+    } else {
+        missVille.textContent = "";
+        }
+    return true;
+}
+function validationCodePostale(event){
+    event.preventDefault();
+    if (codePostale.validity.valueMissing){
+        event.preventDefault();
+        missCodePostale.textContent = "Code postale manquant";
+        return false;
+    console.log(missCodePostale.textContent);
+    } else if (codePostaleValid.test(codePostale.value) == false){
+        event.preventDefault();
+        missCodePostale.textContent = "Format incorrect";
+        return false;
+    } else {
+            
+        }
+    return true;
+}
+function passerCommande(){
+    let cart = sessionStorage.getItem("cart")
+        cart = JSON.parse(cart);
+        missCodePostale.textContent = "";
+        alert("Votre commande a bien prise en compte !");
+        sessionStorage.clear();
+        nom.value="";
+        prenom.value="";
+        courriel.value="";
+        adresse.value="";
+        ville.value="";
+        codePostale.value="";
+        var acheter = document.querySelector(".container");
+        acheter.textContent="";
+        
+        var subtotal = document.querySelector(".subtotal");
+        subtotal.textContent ="";
+
+    console.log("bravo");
+}
+let order = {
+    contact: {
+        firstName: "test1",
+        lastName: "test2",
+        address: "test3",
+        city: "test4",
+        email: "test5"
+    },
+    product:["idtest1", "idtest2", "idtest3", "idtest4", "idtest5"]
+}
